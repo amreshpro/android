7ee3ad1366164816371881aab0ce70045eb042ea
Amresh Maurya <amresh.terminal@gmail.com>
2023-12-29 12:36:25 +0530

    google map activity

---
+++ app/src/main/java/com/example/gmapactivity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.gmapactivity;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
+        // Add a marker in Sydney and move the camera
+        LatLng city = new LatLng(28, 76);
+        googleMap.addMarker(new MarkerOptions().position(city).title("Rohtak"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(city));
+
+
+    }
+}
